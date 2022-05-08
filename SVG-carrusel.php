c12e90e5c21e27f0a3007a33e9abc8391b272713
ImaSalas <92271130+ImaSalas@users.noreply.github.com>
2022-05-07 19:01:41 -0500

    Update SVG-carrusel.php

--- SVG-carrusel.php
+++ SVG-carrusel.php
@@ -5,7 +5,7 @@
 
 <!-- texto -->
 <div id="texto">
-<h1 class="titulo"><em>Hola</em> desde <em>Europa</em></h1>
+<h1 class="titulo"><em>Hola</em> desde <em>Estado Unidos</em></h1>
 <h2 class="titulo">El <em>mundo</em> cacao <em>Nits</em></h2>
 </div>
 <!-- // cerro texto -->
